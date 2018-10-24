afcf2e35329c272852c70fd03a81b02365662c71
unknown <vspsvk@gmail.com>
2018-10-23 22:27:04 -0500

    fixed fetch issue

--- src/util/ServerUtils.js
+++ src/util/ServerUtils.js
@@ -1,15 +1,16 @@
-import {SERVER_BASE_URL} from '../constants';
+import {SERVER_BASE_URL} from '../constants/constants.js';
 
 const request = (options) => {
   const headers = new Headers( {
-    'Access-Control-Allow-Credentials' : 'true',
-    'Access-Control-Allow-Origin' : 'cors_url',
+    //'Access-Control-Allow-Credentials' : 'true',
+    //'Access-Control-Allow-Origin' : 'cors_url',
         'Content-Type': 'application/json',
+         'Accept': 'application/json',
     })
 
     const defaults = {headers: headers};
     options = Object.assign({}, defaults, options);
-
+    console.log(options);
     return fetch(options.url, options)
    .then(response =>
        response.json().then(json => {
@@ -23,9 +24,56 @@ const request = (options) => {
 };
 
 export function saveParty(partySaveRequest) {
-    return request({
-      url : SERVER_BASE_URL+"/saveParty",
-      method : "POST",
-      bodty : JSON.stringify(partySaveRequest)
-    });
+
+  const options = {
+      method: 'post',
+      headers: {
+        'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8'
+      },
+      body: JSON.stringify(partySaveRequest)
+    }
+
+  debugger;
+
+  fetch(SERVER_BASE_URL+"/saveParty", options).catch(err => {
+    console.error('Request failed', err)
+  }).then(response =>
+      response.json().then(json => {
+        console.log(json);
+          if(!response.ok) {
+              return Promise.reject(json);
+          }
+          return json;
+      })
+  );
+
 }
+
+//  event.preventDefault();
+/*
+console.log(SERVER_BASE_URL);
+return fetch(SERVER_BASE_URL+"/saveParty", {
+  method: 'POST',
+  headers: {
+    'Access-Control-Allow-Credentials' : 'true',
+    //'Access-Control-Allow-Origin' : 'cors_url',
+    'Accept': 'application/json',
+    //'Content-Type': 'application/json',
+    'Content-Type':'text/plain',
+  },
+  body: JSON.stringify(partySaveRequest)
+}).then(response =>
+    response.json().then(json => {
+        if(!response.ok) {
+            return Promise.reject(json);
+        }
+        return json;
+    })
+);
+
+*/
+  //  return request({
+  //    url : SERVER_BASE_URL+"/saveParty",
+  //    method : 'POST',
+  //    body : JSON.stringify(partySaveRequest)
+  //  });
